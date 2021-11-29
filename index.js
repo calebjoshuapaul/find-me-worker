1f554e4bdd90f52a2678c955f0c04522a9d86845
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-29 15:16:53 +0530

    get geolocation

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ addEventListener("fetch", (event) => {
 });
 
 async function handleRequest(request) {
-  console.log(request.cf);
-  let htmlContent = `<h1> Country: ${(request.cf || {}).country} </h1>`;
+  console.log(request.cf || {});
+  let htmlContent = `
+  <!DOCTYPE html>
+  <body>
+  <head></head>
+  <h1> Country: ${(request.cf || {}).country} </h1>
+  <p> Colo: ${request.cf.colo || {}}</p>
+  <p> PostalCode: ${request.cf.postalCode || {}} </p>
+  </body>
+  `;
 
   return new Response(htmlContent, {
     headers: { "content-type": "text/plain" },
