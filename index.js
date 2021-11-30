480cc9db9e5ec4f2be41211c3bff7ec474acd599
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-30 21:58:52 +0530

    update worker

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
-console.log(process.env.HELLO);
-
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event));
 });
 
+// https://api.tomtom.com/search/2/nearbySearch/.json?lat=28.479162&lon=77.311131&key=*****
+
+const API_ENDPOINT = "https://api.tomtom.com/search/2/nearbySearch/.json";
+
 async function handleRequest(event) {
   const request = event.request;
-  const res = JSON.stringify(request.cf.city);
 
-  ("Delhi");
+  let latitude = request.cf.latitude;
+  let longitude = request.cf.longitude;
 
-  return new Response(res, {
-    headers: { "content-type": "text/plain" },
+  const places = await fetchNearbyPlaces(latitude, longitude);
+  // console.log(JSON.stringify(places.results));
+
+  const list = places.results.map(function ({ address, poi, dist }) {
+    return {
+      address: address.freeformAddress,
+      name: poi.name,
+      distance: dist,
+      contact: poi.phone ? poi.phone : "No contact info found",
+    };
   });
+  const result = JSON.stringify(list);
+
+  return new Response(result, {
+    headers: { "content-type": "application/json" },
+  });
+}
+
+async function fetchNearbyPlaces(lat, long) {
+  const response = fetch(
+    API_ENDPOINT + `?lat=${lat}&lon=${long}&key=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return response;
 }
